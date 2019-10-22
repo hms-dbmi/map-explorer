cb11cb117d3af9abc8b931b749228af815c48ab9
houchiyee <houchiyee@icloud.com>
2019-10-21 23:41:34 -0400

    Update home.R and add file upload feature

--- Update_Oct2019/home.R
+++ Update_Oct2019/home.R
@@ -1,11 +1,38 @@
 output$pageStub <- renderUI(tagList(
    fluidRow(
       column(10,
-         HTML("<p>This is the home page of our excellent web site.</p>",
-              "<p> You can upload your data here. </p>",
-              "<p><p>There's a second page that displays summarized EHR information both on the overview level, 
-              and on a detailed level with supporting evidence such as different lab measurement for the selected diseases for each patient.</p></p>",
-              "<p> On that page you can ...</p>")
+         h3('This is the home page of our excellent web site.'),
+         h5('You can upload your data in this page.'),
+         h5("Once you upload the data, there will be a second page that displays summarized EHR information both on the overview level, 
+            and on a detailed level with supporting evidence such as different lab measurement for the selected diseases for each patient."),
+         h5('On that page you can ...'),
+         
+         h4("You can upload your own data files here!"),
+         fileInput(inputId = "file1",
+                   label = "Choose file for Phecode info:",
+                   multiple = F,
+                   buttonLabel = "Browse...",
+                   placeholder = "No file selected"),
+         fileInput(inputId = "file2"
+                   , label = "Choose file for Patients and MAP cutoff:"
+                   , multiple = F
+                   , buttonLabel = "Browse..."
+                   , placeholder = "No file selected"),
+         fileInput(inputId = "file3"
+                   , label = "Choose file for encounters of MS patient:"
+                   , multiple = F
+                   , buttonLabel = "Browse..."
+                   , placeholder = "No file selected"),
+         fileInput(inputId = "file4"
+                   , label = "Choose file for Vitamin D values of MS patient:"
+                   , multiple = F
+                   , buttonLabel = "Browse..."
+                   , placeholder = "No file selected"),
+         fileInput(inputId = "file5"
+                   , label = "Choose file for CUIs lists of MS patient:"
+                   , multiple = F
+                   , buttonLabel = "Browse..."
+                   , placeholder = "No file selected")
       )
    )
 ))
